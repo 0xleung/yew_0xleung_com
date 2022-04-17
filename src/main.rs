2673d8debeaa45f8d95962c86d112f6d8df42864
JobinLeung <jobinleung@gmail.com>
2022-04-17 20:36:23 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+            <h1>{ "Hello Yew" }</h1>
+        </>
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
+
