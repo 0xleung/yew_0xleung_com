676277d25c5b64e5c1f6dad4c96a93805c451e22
jobinleung <jobinleung@gmail.com>
2022-05-06 22:29:45 +0800

    chore: sub title

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use yew::prelude::*;
 fn app() -> Html {
     html! {
         <>
-            <h1>{ "Hello Yew, My name is 0xLeung, and I'm a React/Go/Rust/Solidity developer" }</h1>
-        </>
+            <h1>{ "Hello Yew, My name is 0xLeung" }</h1>
+            <p>, and I'm a React/Go/Rust/Solidity developer </p>
+        </p>
     }
 }
 
